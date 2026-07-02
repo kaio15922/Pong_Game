e8f496a4f1c78d54ff2fc2d064a4a230b25675c1
kaio15922 <kaiorenato12345@gmail.com>
2026-07-01 23:12:03 -0300

    feat: primeira tela com Raylib

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
-#include <stdio.h>
+#include <raylib.h>
 
 int main()
 {
-    printf("SALVE SALVE\n");
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+  
+    InitWindow(screenWidth, screenHeight, "Pong");
+    SetTargetFPS(60);
+  
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Espero que não seja difícil de usar.", 150, 225, 25, DARKGRAY);
+        EndDrawing();
+    }
+  
+    CloseWindow();
+    return 0;
 }
